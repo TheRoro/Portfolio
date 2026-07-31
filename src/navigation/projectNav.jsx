43ecf4cfa1d1b681940c9cb27522649efd0ec571
TheRoro <coding@rodrigo.cc>
2026-07-31 09:01:35 -0700

    Improving projects mobile view

--- src/navigation/projectNav.jsx
+++ src/navigation/projectNav.jsx
@@ -5,13 +5,17 @@ import "./styles.scss"
 
 const ProjectNav = () => {
   return (
-    <nav className="nav" aria-label="Primary">
+    <nav className="nav nav-project" aria-label="Primary">
       <Link to="/" className="img-link" aria-label="Home">
         <NavLogo />
       </Link>
       <div className="links">
-        <Link className="navigation-link" to="/projects">
-          All Projects
+        <Link
+          className="navigation-link"
+          to="/projects"
+          aria-label="All Projects"
+        >
+          Projects
         </Link>
         <Link className="navigation-link" to="/">
           Home
